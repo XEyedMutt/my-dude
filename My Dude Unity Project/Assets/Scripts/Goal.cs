17d4b0a97f7cdcfcb75190879d13b1320f9ab02d
XEyedMutt <55261815+XEyedMutt@users.noreply.github.com>
2019-10-24 18:09:25 -0500

    Created a goal (green ball) with trigger and added "You win!" text

---
+++ My Dude Unity Project/Assets/Scripts/Goal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// The Goal detects that when the ball touches the goal, you win the level.
+/// </summary>
+public class Goal : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Touched trigger");
+        if (collision.tag == "Player" )
+        {
+            Debug.Log("The player has entered the goal!");
+        }
+    }
+}
