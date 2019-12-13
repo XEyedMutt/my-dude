3c2965cd77f7727aaedc48dbaa9f0423a516a023
XEyedMutt <55261815+XEyedMutt@users.noreply.github.com>
2019-12-12 18:04:01 -0600

    Add new sprites and started fail code

---
+++ My Dude Unity Project/Assets/Scripts/Fail.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fail : MonoBehaviour
+{
+    public GameObject loseText;
+
+    private AudioSource audiosource;
+
+    private void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+
+
+}
